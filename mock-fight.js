9ab14f065e0329ac087da9a8f07118c0e700fb8d
AJLosey <aaronlosey11@gmail.com>
2022-04-24 01:41:44 -0500

    add file for initializing fight. literally one line of code, but i'm tired

---
+++ mock-fight.js
@@ -0,0 +1,5 @@
+//nothing stored here, just calling the functions to initialize the fight. call gorgo to the html, draw the player's first 5 cards, call the turn handler, ect...
+
+gorgo.fillInfoEnemy();
+
+
